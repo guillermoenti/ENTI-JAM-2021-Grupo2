224711db1368da76fbf58b27b1d6b5c0123f4c24
Pedrofenti <pedro.fernandez@enti.cat>
2021-04-13 11:58:04 +0200

    Mensajes

---
+++ JAM/Assets/Scripts/RollController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RollController : MonoBehaviour
+{
+    Rigidbody2D rigidBody;
+
+    int speed;
+
+    private void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //speed = 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Rotate(0, 0, -0.2f);
+    }
+}
