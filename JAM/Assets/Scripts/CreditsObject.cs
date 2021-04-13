bef6625db98ba7143b62fe4e3870aac6cbf7817b
Pedrofenti <pedro.fernandez@enti.cat>
2021-04-13 19:57:03 +0200

    Creditos

---
+++ JAM/Assets/Scripts/CreditsObject.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreditsObject : MonoBehaviour
+{
+    private Rigidbody2D rigidBody;
+    public float speed = 20.5f;
+    public bool quiet = false;
+    private float esc;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        rigidBody.velocity = -transform.up * speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (quiet)
+        {
+            rigidBody.velocity = Vector3.zero;
+            esc += Time.deltaTime;
+            if (esc > 2) { SceneManager.LoadScene("MainMenu"); }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Coco")
+        {
+            quiet = true;
+        }
+    }
+}
