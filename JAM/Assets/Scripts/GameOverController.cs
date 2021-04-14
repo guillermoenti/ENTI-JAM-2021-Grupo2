d725f3c268da802dff4bf3e5aa38293f4f4e27ce
Guille <guillermo.barrasa@enti.cat>
2021-04-14 10:01:04 +0200

    Arreglado GameObject y Secciones. Añadida lista de records en gameover

--- JAM/Assets/Scripts/GameOverController.cs
+++ JAM/Assets/Scripts/GameOverController.cs
@@ -10,6 +10,8 @@ public class GameOverController : MonoBehaviour
     KeyCode downButton = KeyCode.DownArrow;
     KeyCode select = KeyCode.Space;
 
+    private List<int> records;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +20,8 @@ public class GameOverController : MonoBehaviour
 
         gameObject.transform.position = new Vector2(0, -100);
         menuoptions = 0;
+
+        records = GameManager.GInstance.getRecords();
     }
 
     // Update is called once per frame
